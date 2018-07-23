fcf9a7f9a5ba14c59140d2b9bd614f6da217c1c6
Mahesh <drupal.mahesh@gmail.com>
2018-07-23 17:49:02 +0530

    added list of server details page

--- modules/custom/my_server_upload/src/Controller/MyServerUploadController.php
+++ modules/custom/my_server_upload/src/Controller/MyServerUploadController.php
@@ -3,6 +3,9 @@
 namespace Drupal\my_server_upload\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Database\Database;
+use Drupal\Core\Database\DatabaseException;
 
 class MyServerUploadController extends ControllerBase {
   public function FormDisplay() {
@@ -10,5 +13,35 @@ class MyServerUploadController extends ControllerBase {
       '#title' => 'My Server Page',
       '#markup' => '',
     );
-  }	
+  }
+  
+  public function ServerDetailsList() {
+    $header = array(
+        array('data' => t('Server Name'), 'field' => 'server_name', 'sort' => 'desc'),
+        array('data' => t('Server Type'), 'field' => 'server_type'),
+        array('data' => t('IP Address'), 'field' => 'ip_address'),
+        array('data' => t('Fqdm'), 'field' => 'fqdm'),
+    );
+      $connection = Database::getConnection();
+      $query = $connection->select('server_information', 'si');
+      $query->fields('si', array('server_name', 'server_type', 'ip_address', 'fqdm'));
+	  $table_sort = $query->extend('Drupal\Core\Database\Query\TableSortExtender')->orderByHeader($header);
+      $pager = $table_sort->extend('Drupal\Core\Database\Query\PagerSelectExtender')->limit(10);
+      $result = $pager->execute();
+
+      foreach($result as $row) {
+        $rows[] = array('data' => (array) $row);
+      }
+      $build = array(
+        '#markup' => t('List of server details')
+      );
+      $build['server_table'] = array(
+        '#theme' => 'table', '#header' => $header,
+          '#rows' => $rows
+      );
+     $build['pager'] = array(
+       '#type' => 'pager'
+     );
+      return $build;	 
+  }
 }
